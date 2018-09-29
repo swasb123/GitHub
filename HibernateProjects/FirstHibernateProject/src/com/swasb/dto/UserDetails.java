46c882cae5876def1825295cb77a24903bdfe613
swasb123 <online.swas@gmail.com>
2018-09-29 16:46:20 +0530

    Hibernate test codes
    Hibernate and Spring Boot test codes

---
+++ HibernateProjects/FirstHibernateProject/src/com/swasb/dto/UserDetails.java
@@ -0,0 +1,29 @@
+package com.swasb.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class UserDetails {
+
+	@Id
+	private int userID;
+	private String userName;
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
